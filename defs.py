47568cf8923bb92f26535357859ca706005bb45f
allex <st-allex@yandex.ru>
2025-01-14 16:43:23 +0300

    get_numbers_digits a recursive function

--- defs.py
+++ defs.py
@@ -1,11 +1,7 @@
-def ret_append(l, e):
-    l.append(e)
-    return l
-
 def get_numbers_digits(n):
     if not isinstance(n, int):
         return 'ERROR: only integer may be parameter here'
-    return ret_append(get_numbers_digits(n//10), n%10) if n//10>0 else [n%10]
+    return get_numbers_digits(n//10)+[n%10] if n//10>0 else [n%10]
 
 n = 123456
 print(get_numbers_digits(n))
